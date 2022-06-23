fbe2e14ca726e5a73202868871259ed6c23d2990
javaMercy <nextelonmusk01@gmail.com>
2022-06-23 21:11:11 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager =
+                new ProductManager(new InMemoryProductDal());
+
+            foreach (Product product in productManager.GetAll())
+            {
+                Console.WriteLine("{0} {1}", product.ProductId, product.ProductName);
+            }
+        }
+    }
+}
