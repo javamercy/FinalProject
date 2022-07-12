5f3ad7bcc7f4bf8a62a669b8e25898151cf6fda7
javaMercy <nextelonmusk01@gmail.com>
2022-07-12 15:48:08 +0300

    Auth problems been solved

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -29,7 +29,7 @@ namespace Business.Concrete
 
         }
 
-        [SecuredOperation("product.add")]
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
